f2e31ee2a0ab38574f9e76c58e9037cd2412b33d
sebousan <sebousan@gmail.com>
2022-11-24 17:12:56 +0100

    postcss

--- postcss.config.js
+++ postcss.config.js
@@ -8,12 +8,12 @@ module.exports = {
         '@fullhuman/postcss-purgecss': {
             mode: 'all',
             content: ['./hugo_stats.json'],
-            safelist: {
-                standard: [
-                    'show',
-                    'fade'
-                ]
-            },
+            // safelist: {
+            //     standard: [
+            //         'show',
+            //         'fade'
+            //     ]
+            // },
             defaultExtractor: (content) => {
                 let els = JSON.parse(content).htmlElements;
                 els = els.tags.concat(els.classes, els.ids);
